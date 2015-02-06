79289e4efed9f9a079182614ffbed3af6725d70f
Andrew <alphadeltablue@users.noreply.github.com>
2015-02-05 19:41:50 -0600

    Updated SelectionSorter.java

--- src/algorithm/SelectionSorter.java
+++ src/algorithm/SelectionSorter.java
@@ -50,6 +50,46 @@ public class SelectionSorter extends Sorter {
 	 * @param arr
 	 *            Array to be sorted
 	 */
+	public static <T extends Comparable<? super T>> T[] selectionSort (T[] arr) {
+		
+		// Initializes variables used for sorting
+		
+		int smallIndex; // Index of the smallest number
+		int pass; // Current sorting pass
+		int j; // Index of smaller loop
+		int length = arr.length; // Length of array
+		T temp;
+		
+		// Loop.
+		// The first pass finds the smallest, the second pass finds the second
+		// smallest, etc.
+		// Will not be affected by duplicates
+		for (pass = 0; pass < (length - 1); pass++) {
+			
+			// Sets the index of the smallest to the current pass
+			smallIndex = pass;
+			
+			// Finds the smallest element in the pass
+			for (j = pass + 1; j < length; j++) {
+				
+				if (arr[j].compareTo(arr[smallIndex]) > 0) {
+					
+					smallIndex = j;
+					
+				}
+				
+			}
+			// Switches the nth smallest number with the nth number
+			temp = arr[pass];
+			arr[pass] = arr[smallIndex];
+			arr[smallIndex] = temp;
+			
+		}
+		
+		// Returns the array
+		return arr;
+		
+	}
 
 	public static void selectionSort(int[] arr) {
 
